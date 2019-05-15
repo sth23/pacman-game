95f7272095c5ef97f7c635ffe0a845f6d64c1f99
sth23 <47009471+sth23@users.noreply.github.com>
2019-05-15 13:03:30 -0400

    Updated from Brython Server: 5/15/2019 1:03:30 PM

--- pacman-game.py
+++ pacman-game.py
@@ -178,7 +178,7 @@ class PacmanGame(App):
         for x in range(0, self.blockcolumns):
             for y in range(0, self.blockrows):
                 Wall(self.blocks, (x * self.lanewidth + 70, y * self.lanewidth + 70))
-                Dots((x * self.lanewidth + 115, y * self.lanewidth + 115))
+                Dots((x * self.lanewidth + 125, y * self.lanewidth + 125))
         
     def step(self):
         self.player1.step()
