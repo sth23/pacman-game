bf98b5dc94b6284664029c45d6c58c92aabbc87c
sth23 <47009471+sth23@users.noreply.github.com>
2019-05-15 11:04:05 -0400

    Updated from Brython Server: 5/15/2019 11:04:01 AM

--- pacman-game.py
+++ pacman-game.py
@@ -18,7 +18,7 @@ blue = Color(0x0000ff, 1.0)
 purple = Color(0x800080, 1.0)
 black = Color(0x000000, 1.0)
 white = Color(0xffffff, 1.0)
-gray = Color(0x999999, 0.5)
+gray = Color(0x888888, 0.5)
 noline = LineStyle(0, black)
 whiteline = LineStyle(1, white)
 
