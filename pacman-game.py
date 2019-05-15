45397091898e6aaae73dfd739a39361b06a25b3e
sth23 <47009471+sth23@users.noreply.github.com>
2019-05-15 12:33:52 -0400

    Updated from Brython Server: 5/15/2019 12:33:51 PM

--- pacman-game.py
+++ pacman-game.py
@@ -199,6 +199,7 @@ class PacmanGame(App):
         # Handle player colliding with ghosts
         if self.player1.collidingWithSprites(Ghost):
             self.player1.destroy()
+            self.quit()
         
 myapp = PacmanGame()
 myapp.run()
