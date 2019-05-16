7e7a5753e9e708b9680e4c5db64ca24684b080a1
sth23 <47009471+sth23@users.noreply.github.com>
2019-05-16 09:13:18 -0400

    Updated from Brython Server: 5/16/2019 9:13:17 AM

--- pacman-game.py
+++ pacman-game.py
@@ -158,6 +158,10 @@ class PacmanGame(App):
         bg_asset = RectangleAsset(self.width, self.height, noline, black)
         bg = Sprite(bg_asset, (0,0))
         
+        # Variable to handle pausing / unpausing
+        self.paused = True
+        PacmanGame.listenKeyEvent("keydown", "space", self.pause)
+        
         # Create player
         self.player1 = Pacman((self.width / 2 - 30, 15))
         self.score = 0
@@ -212,53 +216,58 @@ class PacmanGame(App):
             self.ghostcount += 1
         self.ghostcount = 1
         
+    def pause(self, event):
+        self.paused = not self.paused
+        
     def step(self):
-        self.player1.step()
-        
-        # Handle ghosts colliding w/ walls and other ghosts
-        for ghost in self.getSpritesbyClass(Ghost):
-            if ghost.collidingWithSprites(Ghost):
-                ghost.hitGhost()
-            if ghost.collidingWithSprites(Wall):
-                ghost.hitWall()
-            ghost.step()
-        
-        # Handle player colliding with walls    
-        if self.player1.collidingWithSprites(Wall):
-            self.player1.hitWall()
+        if self.paused == False:
+            self.player1.step()
             
-        # Handle player eating dots
-        for dot in self.player1.collidingWithSprites(Dot):
-            dot.destroy()
-            self.dotcount -= 1
-            self.score += 10
-            print("Score: " + str(self.score))
+            # Handle ghosts colliding w/ walls and other ghosts
+            for ghost in self.getSpritesbyClass(Ghost):
+                if ghost.collidingWithSprites(Ghost):
+                    ghost.hitGhost()
+                if ghost.collidingWithSprites(Wall):
+                    ghost.hitWall()
+                ghost.step()
             
-        # Handle player colliding with ghosts
-        if self.player1.collidingWithSprites(Ghost):
-            if self.player1.gameover == False:
+            # Handle player colliding with walls    
+            if self.player1.collidingWithSprites(Wall):
+                self.player1.hitWall()
+                
+            # Handle player eating dots
+            for dot in self.player1.collidingWithSprites(Dot):
+                dot.destroy()
+                self.dotcount -= 1
+                self.score += 10
+                print("Score: " + str(self.score))
+                
+            # Handle player colliding with ghosts
+            if self.player1.collidingWithSprites(Ghost):
+                if self.player1.gameover == False:
+                    self.player1.x = self.width / 2 - 30
+                    self.player1.y = 15
+                    self.player1.vx = 0
+                    self.player1.vy = 0
+                    self.resetGhosts()
+                    if self.extralives == 0:
+                        self.player1.gameover = True
+                        print("Game Over")
+                    else:
+                        self.extralives -= 1
+                        print("Extra Lives: " + str(self.extralives))
+                
+            # Reset board @ end of level
+            if self.dotcount == 0:
                 self.player1.x = self.width / 2 - 30
                 self.player1.y = 15
                 self.player1.vx = 0
                 self.player1.vy = 0
+                self.makeDots()
                 self.resetGhosts()
-                if self.extralives == 0:
-                    self.player1.gameover = True
-                    print("Game Over")
-                else:
-                    self.extralives -= 1
-                    print("Extra Lives: " + str(self.extralives))
-            
-        # Reset board @ end of level
-        if self.dotcount == 0:
-            self.player1.x = self.width / 2 - 30
-            self.player1.y = 15
-            self.player1.vx = 0
-            self.player1.vy = 0
-            self.makeDots()
-            self.resetGhosts()
-            for ghost in self.getSpritesbyClass(Ghost):
-                ghost.speed = ghost.speed + 0.5
+                for ghost in self.getSpritesbyClass(Ghost):
+                    ghost.speed = ghost.speed + 0.5
+                self.paused = not self.paused
         
 myapp = PacmanGame()
 myapp.run()
